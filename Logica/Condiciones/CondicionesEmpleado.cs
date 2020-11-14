05fea9501952fa37bcf35999d75d3338572474e8
Santiago <s.alvarado0@icloud.com>
2020-11-14 15:19:58 -0300

    F por logica

--- Logica/Condiciones/CondicionesEmpleado.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Logica.Condiciones
-{
-    public class CondicionesEmpleado
-    {
-        public double aEmpleado { get; internal set; }
-        public double bEmpleado { get; internal set; }
-    }
-}
